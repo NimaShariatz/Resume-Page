b1321dde53bdc7ca3181d6f57ed01b48fce52044
NimaShariatz <shariatz@ualberta.ca>
2024-08-30 00:25:25 -0600

    Education sorted

--- effects.js
+++ effects.js
@@ -1,3 +1,4 @@
+"use strict"
 var options = document.getElementsByClassName("option_svg");
 
 var nodes = document.getElementsByClassName("node");
@@ -8,7 +9,7 @@ var timeline_sections = document.getElementsByClassName("timeline_sect");
 
 
 var delay = 200;
-for (i = 0; i < nodes.length; i++) {
+for (var i,i = 0; i < nodes.length; i++) {
     nodes[i].style.outline = "var(--border_thickness) solid var(--color4)";
     timeline_sections[i].style.opacity = "1";
     timeline_sections[i].style.transitionDelay = delay.toString() + "ms";
@@ -23,10 +24,10 @@ const sections = [document.getElementById("Education_section"), document.getElem
     document.getElementById("Aboutme_section")
 ];
 
-options[0].classList.add("chosen_option");//so education starts as white
-sections[0].classList.add("appear")
-sections[0].style.visibility="visible";
-sections[0].style.opacity="1";
+options[0].classList.add("chosen_option");//the starting set
+sections[0].classList.add("appear");//the starting set
+sections[0].style.visibility="visible";//the starting set
+sections[0].style.opacity="1";//the starting set
 
 var currentSection = 0;
 var previousSection = 0;
@@ -42,6 +43,7 @@ function display_new(current){
     sections[current].classList.add("appear");
     sections[current].style.visibility = "visible";
     sections[current].style.opacity = "1";
+
 
     sections[previousSection].classList.add("dissappear");
     sections[previousSection].style.visibility = "hidden";
